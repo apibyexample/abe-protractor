6bd19ad7d6de29d00de3b43c571d8efc8f7a2fa9
Nick White <nick.white@rockabox.com>
2014-09-11 10:32:24 +0100

    Move app.js to src/abe-protractor.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
                 files: {
                     src: [
                         'Gruntfile.js',
-                        'app.js'
+                        'src/**/*.js'
                     ]
                 }
             }
