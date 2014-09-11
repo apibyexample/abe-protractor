f75dbfa24cc2d030634d9b776d47e5fbe8626142
Nick White <nick.white@rockabox.com>
2014-09-11 16:11:40 +0100

    Generate default stubs based on abe-spec
    - Generate a generic stub based on abe-spec
    - Checks if a stub has been manually created and uses that instead
    - Closes #1

--- src/abe-protractor.js
+++ src/abe-protractor.js
@@ -1,8 +1,44 @@
 var glob = require('glob'),
     path = require('path'),
-    util = require('util');
+    util = require('util'),
+    fs = require('fs');
 
 exports.setupServiceStubs = function (options) {
+    var defaultStub = function (data) {
+            angular
+                .module('app.stubs.line-item-ad-unit', [
+                    'ngMockE2E',
+                    'app'
+                ])
+                .run(function ($httpBackend) {
+                    var createStub = function (method, example, $httpBackend) {
+                            var httpMethod = method.toUpperCase(),
+                                url = new RegExp(example.request.url),
+                                res = example.response;
+
+                            $httpBackend
+                                ['when' + httpMethod](url)
+                                .respond(function (method, url) {
+                                    return [res.status, res.body];
+                                });
+                        },
+                        methodData,
+                        method;
+
+                    for (methodData in data) {
+                        var httpMethod = data[methodData].method,
+                            examples = data[methodData].examples;
+
+                        for (method in examples) {
+                            createStub(
+                                httpMethod,
+                                examples[method],
+                                $httpBackend
+                            );
+                        }
+                    }
+                });
+        };
 
     // Loop over all JSON mock data folders pased to the setupServiceStubs
     glob
@@ -25,9 +61,16 @@ exports.setupServiceStubs = function (options) {
                 moduleName = util.format('app.stubs.%s', folder),
                 stubPath = util
                     .format(options.stubsLocation + '%s.stub.js', folder),
-                stub = require(process.cwd() + stubPath),
+                stubFullPath = process.cwd() + stubPath,
+                stub,
                 data = {},
                 methods = [];
+
+            if (fs.existsSync(stubFullPath)) {
+                stub = require(stubFullPath);
+            } else {
+                stub = defaultStub;
+            }
 
             // Require each of the JSON files in the folder into a object hash
             // keyed by filename sans file extention, yielding an object with
